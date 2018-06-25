a0964c697a5461e4c34ed127f33c9dfa7d9e5383
Tinedanzer <johnhillallen@gmail.com>
2018-06-25 02:14:36 -0500

    late commit!

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Psychic-Game
+<!-- homework notes pseudo code/readme
+
+press any key to start game, 
+use a function to a pick random letter and change guesses to 12;
+
+press letter to guess
+
+for:  10 guesses
+
+if guess is incorrect, -1 to guess counter, but to keep it fair, you get to keep guessing.
+
+if guess is correct, +1 to win counter
+
+Once guess counter hits 0,  +1 to lose counter
+
+After user wins;
+run computerChoice to pick a new letter and change guesses to 10.
+
+
+
+# Psychic-Game -->
---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+// begin JS
+
+var letters= ["a","b","c","d","e","f","g","h", "i", "j", "k", "l", "m", "n", "o", "p",
+"q","r","s","t","u","v","w","x","y","z"];
+var wins = 0;
+var losses = 0;
+var left = 10;
+
+// console.log;
+// document.getElementById("wins");
+// document.getElementById("losses");
+// document.getElementById("left");
+// document.getElementById("guesses");
+
+function happy(){for (var i=0; i < letters.length; i=i+1)
+  {
+    console.log ("I love" + " " + letters[i]);}}
+happy();
+
+document.onkeypress = function(event) {
+  var userGuess = event.key;
+  var node =document.createElement("li");
+  var textnode =document.createTextNode(event.key);
+  node.appendChild(textnode);
+  document.getElementById("guesses").appendChild(node);
+//   document.getElementById('guesses').innerHTML= userGuess + ",";
+  var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+
+  if(userGuess === computerChoice){
+      wins++;
+      computerChoice;
+      console.log(computerChoice);
+      document.getElementById('guesses').innerHTML= userGuess
+      alert("YOU WIN!!!!!!!")
+  }
+  else{
+      left--;
+  }
+  if(left === 0){
+      losses++;
+      left=10;
+  }
+
+document.getElementById('left').innerHTML = left;
+document.getElementById('wins').innerHTML = wins;
+document.getElementById('losses').innerHTML = losses;
+};
